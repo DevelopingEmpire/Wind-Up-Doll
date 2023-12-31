c74815007df0fa8b59731a771ea21c17240ffeec
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-01-01 04:13:13 +0900

    add demo file

---
+++ Assets/Scripts/NetworkInputData.cs
@@ -0,0 +1,22 @@
+﻿using Fusion;
+
+enum Buttons
+{
+    forward = 0,
+    back = 1,
+    right = 2,
+    left =3,
+}
+
+// string -> NetworkString
+// bool -> NetworkBool
+// float -> angle 
+
+public struct NetworkInputData : INetworkInput
+{
+    public NetworkButtons buttons;
+    public Angle yaw;
+    public Angle pitch;
+
+    
+}
