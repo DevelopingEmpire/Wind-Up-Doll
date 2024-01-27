de01114122d596f9d23ed98d132b34d5855cf447
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-01-27 19:50:02 +0900

    Update PlayerTrigger.cs

--- Assets/Scripts/Fusion/PlayerTrigger.cs
+++ Assets/Scripts/Fusion/PlayerTrigger.cs
@@ -5,11 +5,15 @@ using Fusion;
 using System.Globalization;
 using StarterAssets;
 using Cinemachine;
+using static Fusion.NetworkCharacterController;
 
 public class PlayerTrigger : NetworkBehaviour
 {
     [SerializeField] NetworkTransform _root;
+    [SerializeField] private float pushPower = 2.0f; // 박스를 밀 때의 힘
     bool _isTele = false;
+    bool _isPushBox = false;
+
 
     public override void FixedUpdateNetwork() // RPC 쓰는게 아니라 이동은 FixedUpdateNetwork에서 처리
     {
@@ -18,20 +22,36 @@ public class PlayerTrigger : NetworkBehaviour
             _root.TeleportToPosition(GameManager.instance.targetPosition.position);
             _isTele = false;
         }
+        if (_isPushBox)
+        {
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger");
         if (other.CompareTag("TeleBox"))
         {
             _isTele = true;
         }
-        else
-        {
-            Debug.Log("Not TeleBox");
-            Debug.Log(other.gameObject);
-        }
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "MoveBox")
+        {
+            Rigidbody body = hit.collider.attachedRigidbody;
+            // 박스에 Rigidbody가 없거나, Kinematic이면 무시
+            if (body == null || body.isKinematic)
+            {
+                return;
+            }
+            // 박스를 밀어내는 방향 계산
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            // 박스에 힘을 추가하여 밀어냄
+            body.velocity = pushDir * pushPower;
+        }
+    }
 }
+
